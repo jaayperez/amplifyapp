81db107c18d4f545b302555a035a1805a9d0b62b
jaayperez <justin@startrev.com>
2021-07-10 16:50:43 -0700

    Configure the React project with Amplify resources

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
